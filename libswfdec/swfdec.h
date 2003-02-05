161564b5b5dedabb1221fd98fcad96f52a4f8e3f
David Schleef <ds@schleef.org>
2003-02-05 20:29:02 +0000

    remove enable/disable_render, pondering API

--- libswfdec/swfdec.h
+++ libswfdec/swfdec.h
@@ -37,8 +37,6 @@ int swfdec_decoder_get_image_size(SwfdecDecoder *s, int *width, int *height);
 int swfdec_decoder_set_colorspace(SwfdecDecoder *s, int colorspace);
 int swfdec_decoder_set_image_size(SwfdecDecoder *s, int width, int height);
 int swfdec_decoder_set_debug_level(SwfdecDecoder *s, int level);
-void swfdec_decoder_enable_render(SwfdecDecoder *s);
-void swfdec_decoder_disable_render(SwfdecDecoder *s);
 
 void *swfdec_decoder_get_sound_chunk(SwfdecDecoder *s, int *length);
 

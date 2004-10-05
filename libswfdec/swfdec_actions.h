cf3172cf4b2c83e466266d4a3c1ba1a02afe6022
David Schleef <ds@schleef.org>
2004-10-05 04:11:41 +0000

    new file

---
+++ libswfdec/swfdec_actions.h
@@ -0,0 +1,10 @@
+
+#ifndef __SWFDEC_ACTIONS_H__
+#define __SWFDEC_ACTIONS_H__
+
+#include <swfdec_decoder.h>
+#include <swfdec_buffer.h>
+
+int swfdec_action_script_execute (SwfdecDecoder * s, SwfdecBuffer *buffer);
+
+#endif

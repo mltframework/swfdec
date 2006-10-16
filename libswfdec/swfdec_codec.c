49fecd10e0b761d781cced560600b761777acc93
Benjamin Otte <lvs@lvs.localdomain>
2006-10-16 18:27:19 +0200

    implement uncompressed sound

--- libswfdec/swfdec_codec.c
+++ libswfdec/swfdec_codec.c
@@ -15,6 +15,82 @@ extern const SwfdecCodec swfdec_codec_ffmpeg_adpcm;
 extern const SwfdecCodec swfdec_codec_ffmpeg_mp3;
 #endif
 
+/*** UNCOMPRESSED SOUND ***/
+
+static gpointer
+swfdec_codec_uncompressed_init (gboolean width, guint channels, guint rate_multiplier)
+{
+  guint ret = rate_multiplier;
+  if (channels == 2)
+    ret *= 2;
+  if (!width)
+    ret |= 0x100;
+  return GUINT_TO_POINTER (ret);
+}
+
+static SwfdecBuffer *
+swfdec_codec_uncompressed_decode_8bit (SwfdecBuffer *buffer, guint multiply)
+{
+  SwfdecBuffer *ret = swfdec_buffer_new_and_alloc (buffer->length * multiply * 2);
+  gint16 *out = (gint16 *) ret->data;
+  guint8 *in = buffer->data;
+  guint count = buffer->length;
+  guint i, j;
+
+  for (i = 0; i < count; i++) {
+    gint16 tmp = ((gint16) *in << 8) ^ (-1);
+    for (j = 0; j < multiply; j++) {
+      *out++ = tmp;
+    }
+    in++;
+  }
+  return ret;
+}
+
+static SwfdecBuffer *
+swfdec_codec_uncompressed_decode_16bit (SwfdecBuffer *buffer, guint multiply)
+{
+  SwfdecBuffer *ret = swfdec_buffer_new_and_alloc (buffer->length * multiply);
+  gint16 *out = (gint16 *) ret->data;
+  gint16 *in = (gint16 *) buffer->data;
+  guint count = buffer->length / 2;
+  guint i, j;
+
+  for (i = 0; i < count; i++) {
+    for (j = 0; j < multiply; j++) {
+      *out++ = GINT16_FROM_LE (*in);
+    }
+    in++;
+  }
+  return ret;
+}
+
+static SwfdecBuffer *
+swfdec_codec_uncompressed_decode (gpointer codec_data, SwfdecBuffer *buffer)
+{
+  guint data = GPOINTER_TO_UINT (codec_data);
+  if (data == 1) {
+    swfdec_buffer_ref (buffer);
+    return buffer;
+  } else if (data & 0x100) {
+    return swfdec_codec_uncompressed_decode_8bit (buffer, data & 0xFF);
+  } else {
+    return swfdec_codec_uncompressed_decode_16bit (buffer, data);
+  }
+}
+
+static void
+swfdec_codec_uncompressed_finish (gpointer codec_data)
+{
+  return;
+}
+
+static const SwfdecCodec swfdec_codec_uncompressed = {
+  swfdec_codec_uncompressed_init,
+  swfdec_codec_uncompressed_decode,
+  swfdec_codec_uncompressed_finish,
+};
+
 /*** PUBLIC API ***/
 
 const SwfdecCodec *
@@ -23,8 +99,7 @@ swfdec_codec_get_audio (SwfdecAudioFormat format)
   switch (format) {
     case SWFDEC_AUDIO_FORMAT_UNDEFINED:
     case SWFDEC_AUDIO_FORMAT_UNCOMPRESSED:
-      SWFDEC_ERROR ("uncompressed sound is not implemented yet");
-      return NULL;
+      return &swfdec_codec_uncompressed;
     case SWFDEC_AUDIO_FORMAT_ADPCM:
 #ifdef HAVE_FFMPEG
       return &swfdec_codec_ffmpeg_adpcm;

1df651f3fe6e7aaa788153718774ab5e2535c7aa
Benjamin Otte <otte@gnome.org>
2006-10-27 19:06:22 +0200

    allow extracting Graphic objects

--- test/swfdec-extract.c
+++ test/swfdec-extract.c
@@ -21,12 +21,109 @@
 #include "config.h"
 #endif
 
+#include <math.h>
 #include <stdlib.h>
+#include <string.h>
+#include <cairo.h>
+#ifdef CAIRO_HAS_SVG_SURFACE
+#  include <cairo-svg.h>
+#endif
+#ifdef CAIRO_HAS_PDF_SURFACE
+#  include <cairo-pdf.h>
+#endif
 #include <libswfdec/swfdec.h>
+#include <libswfdec/color.h>
+#include <libswfdec/swfdec_button.h>
+#include <libswfdec/swfdec_graphic.h>
 #include <libswfdec/swfdec_player_internal.h>
 #include <libswfdec/swfdec_root_movie.h>
 #include <libswfdec/swfdec_sound.h>
+#include <libswfdec/swfdec_sprite.h>
 #include <libswfdec/swfdec_swf_decoder.h>
+
+static gboolean
+export_sound (SwfdecSound *sound, const char *filename)
+{
+  GError *error = NULL;
+
+  if (sound->decoded == NULL) {
+    g_printerr ("sound is not decoded");
+    return FALSE;
+  }
+  if (!g_file_set_contents (filename, (char *) sound->decoded->data, 
+	sound->decoded->length, &error)) {
+    g_printerr ("Couldn't save sound to file \"%s\": %s\n", filename, error->message);
+    g_error_free (error);
+    return FALSE;
+  }
+  return TRUE;
+}
+
+static cairo_surface_t *
+surface_create_for_filename (const char *filename, int width, int height)
+{
+  guint len = strlen (filename);
+  cairo_surface_t *surface;
+  if (FALSE) {
+#ifdef CAIRO_HAS_PDF_SURFACE
+  } else if (len >= 3 && g_ascii_strcasecmp (filename + len - 3, "pdf") == 0) {
+    surface = cairo_pdf_surface_create (filename, width, height);
+#endif
+#ifdef CAIRO_HAS_SVG_SURFACE
+  } else if (len >= 3 && g_ascii_strcasecmp (filename + len - 3, "svg") == 0) {
+    surface = cairo_svg_surface_create (filename, width, height);
+#endif
+  } else {
+    surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, width, height);
+  }
+  return surface;
+}
+
+static gboolean
+surface_destroy_for_type (cairo_surface_t *surface, const char *filename)
+{
+  if (cairo_surface_get_type (surface) == CAIRO_SURFACE_TYPE_IMAGE) {
+    cairo_status_t status = cairo_surface_write_to_png (surface, filename);
+    if (status != CAIRO_STATUS_SUCCESS) {
+      g_printerr ("Error saving file: %s\n", cairo_status_to_string (status));
+      cairo_surface_destroy (surface);
+      return FALSE;
+    }
+  }
+  cairo_surface_destroy (surface);
+  return TRUE;
+}
+
+static gboolean
+export_graphic (SwfdecGraphic *graphic, const char *filename)
+{
+  cairo_surface_t *surface;
+  cairo_t *cr;
+  guint width, height;
+  const SwfdecColorTransform trans = { { 1, 1, 1, 1 }, { 0, 0, 0, 0 } };
+
+  if (SWFDEC_IS_SPRITE (graphic)) {
+    g_printerr ("Sprites can not be exported");
+    return FALSE;
+  }
+  if (SWFDEC_IS_BUTTON (graphic)) {
+    g_printerr ("Buttons can not be exported");
+    return FALSE;
+  }
+  width = ceil (graphic->extents.x1 / SWFDEC_SCALE_FACTOR) 
+    - floor (graphic->extents.x0 / SWFDEC_SCALE_FACTOR);
+  height = ceil (graphic->extents.y1 / SWFDEC_SCALE_FACTOR) 
+    - floor (graphic->extents.y0 / SWFDEC_SCALE_FACTOR);
+  surface = surface_create_for_filename (filename, width, height);
+  cr = cairo_create (surface);
+  cairo_translate (cr, - floor (graphic->extents.x0 / SWFDEC_SCALE_FACTOR),
+    - floor (graphic->extents.y0 / SWFDEC_SCALE_FACTOR));
+  cairo_scale (cr, 1 / SWFDEC_SCALE_FACTOR, 1 / SWFDEC_SCALE_FACTOR);
+  swfdec_graphic_render (graphic, cr, &trans, &graphic->extents, TRUE);
+  cairo_show_page (cr);
+  cairo_destroy (cr);
+  return surface_destroy_for_type (surface, filename);
+}
 
 static void
 usage (const char *app)
@@ -37,7 +134,8 @@ usage (const char *app)
 int
 main (int argc, char *argv[])
 {
-  SwfdecSound *sound;
+  SwfdecCharacter *character;
+  int ret = 0;
   SwfdecPlayer *player;
   GError *error = NULL;
   guint id;
@@ -62,26 +160,23 @@ main (int argc, char *argv[])
     return 1;
   }
   id = strtoul (argv[2], NULL, 0);
-  sound = (SwfdecSound *) swfdec_swf_decoder_get_character (
+  character = swfdec_swf_decoder_get_character (
       SWFDEC_SWF_DECODER (SWFDEC_ROOT_MOVIE (player->roots->data)->decoder),
       id);
-  if (!SWFDEC_IS_SOUND (sound) || sound->decoded == NULL) {
-    g_printerr ("id %u does not reference a sound object", id);
-    g_object_unref (player);
-    player = NULL;
-    return 1;
+  if (SWFDEC_IS_SOUND (character)) {
+    if (!export_sound (SWFDEC_SOUND (character), argv[3]))
+      ret = 1;
+  } else if (SWFDEC_IS_GRAPHIC (character)) {
+    if (!export_graphic (SWFDEC_GRAPHIC (character), argv[3]))
+      ret = 1;
+  } else {
+    g_printerr ("id %u does not specify an exportable object", id);
+    ret = 1;
   }
-  if (!g_file_set_contents (argv[3], (char *) sound->decoded->data, 
-	sound->decoded->length, &error)) {
-    g_printerr ("Couldn't save sound to file \"%s\": %s\n", argv[1], error->message);
-    g_error_free (error);
-    g_object_unref (player);
-    player = NULL;
-    return 1;
-  }
+
   g_object_unref (player);
   player = NULL;
 
-  return 0;
+  return ret;
 }
 

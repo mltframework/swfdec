6a00ab167bdaee6051fae34f1cea47b8e0d15b89
Benjamin Otte <lvs@lvs.(none)>
2006-12-03 14:47:39 +0100

    add list of scripts to the debugger

--- player/swfdebug.c
+++ player/swfdebug.c
@@ -1,6 +1,8 @@
 #include <gtk/gtk.h>
 #include <math.h>
 #include <libswfdec/swfdec.h>
+#include <libswfdec/swfdec_debugger.h>
+#include "swfdec_debug_scripts.h"
 #include "swfdec_widget.h"
 #ifdef CAIRO_HAS_SVG_SURFACE
 #include <cairo-svg.h>
@@ -42,28 +44,41 @@ iterate (gpointer dec)
 }
 
 static void
-view_swf (SwfdecPlayer *dec, double scale, gboolean use_image)
+view_swf (SwfdecPlayer *player, double scale, gboolean use_image)
 {
-  GtkWidget *window, *widget, *vbox;
+  GtkWidget *window, *widget, *hpaned, *vbox;
 
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  
+  hpaned = gtk_hpaned_new ();
+  gtk_container_add (GTK_CONTAINER (window), hpaned);
+
+  /* left side */
   vbox = gtk_vbox_new (FALSE, 3);
-  widget = swfdec_widget_new (dec);
+  gtk_paned_add1 (GTK_PANED (hpaned), vbox);
+
+  widget = swfdec_debug_scripts_new (player);
+  gtk_box_pack_start (GTK_BOX (vbox), widget, TRUE, TRUE, 0);
+
+  /* right side */
+  vbox = gtk_vbox_new (FALSE, 3);
+  gtk_paned_add2 (GTK_PANED (hpaned), vbox);
+
+  widget = swfdec_widget_new (player);
   swfdec_widget_set_scale (SWFDEC_WIDGET (widget), scale);
   swfdec_widget_set_use_image (SWFDEC_WIDGET (widget), use_image);
   gtk_box_pack_start (GTK_BOX (vbox), widget, TRUE, TRUE, 0);
 
   widget = gtk_button_new_with_mnemonic ("_Iterate");
-  g_signal_connect_swapped (widget, "clicked", G_CALLBACK (iterate), dec);
+  g_signal_connect_swapped (widget, "clicked", G_CALLBACK (iterate), player);
   gtk_box_pack_end (GTK_BOX (vbox), widget, FALSE, TRUE, 0);
 
 #ifdef CAIRO_HAS_SVG_SURFACE
   widget = gtk_button_new_with_mnemonic ("_Save current frame");
-  g_signal_connect (widget, "clicked", G_CALLBACK (save_to_svg), dec);
+  g_signal_connect (widget, "clicked", G_CALLBACK (save_to_svg), player);
   gtk_box_pack_end (GTK_BOX (vbox), widget, FALSE, TRUE, 0);
 #endif
 
-  gtk_container_add (GTK_CONTAINER (window), vbox);
   g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
   gtk_widget_show_all (window);
 
@@ -75,6 +90,7 @@ main (int argc, char *argv[])
 {
   int ret = 0;
   double scale;
+  SwfdecLoader *loader;
   SwfdecPlayer *player;
   GError *error = NULL;
   gboolean use_image = FALSE;
@@ -106,12 +122,14 @@ main (int argc, char *argv[])
     return 1;
   }
 
-  player = swfdec_player_new_from_file (argv[1], &error);
-  if (player == NULL) {
+  loader = swfdec_loader_new_from_file (argv[1], &error);
+  if (loader == NULL) {
     g_printerr ("Couldn't open file \"%s\": %s\n", argv[1], error->message);
     g_error_free (error);
     return 1;
   }
+  player = swfdec_player_new_with_debugger ();
+  swfdec_player_set_loader (player, loader);
   if (swfdec_player_get_rate (player) == 0) {
     g_printerr ("File \"%s\" is not a SWF file\n", argv[1]);
     g_object_unref (player);

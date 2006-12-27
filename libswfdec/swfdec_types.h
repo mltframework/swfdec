6149f5f98b10b77d4f64d745155b4a195ab2f69b
Benjamin Otte <otte@gnome.org>
2006-12-27 14:10:38 +0100

    remove old swfdec_actions.h

--- libswfdec/swfdec_types.h
+++ libswfdec/swfdec_types.h
@@ -17,7 +17,6 @@ typedef int SwfdecFixed;
 #define SWFDEC_FIXED_TO_INT(f) ((f) / SWFDEC_FIXED_SCALE_FACTOR)
 #define SWFDEC_INT_TO_FIXED(i) ((i) * SWFDEC_FIXED_SCALE_FACTOR)
 
-typedef struct _SwfdecActionContext SwfdecActionContext;
 typedef struct _SwfdecButton SwfdecButton;
 typedef struct _SwfdecCache SwfdecCache;
 typedef struct _SwfdecCharacter SwfdecCharacter;

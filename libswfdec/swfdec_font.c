8a164b9aeff20d0b77f2225135b688cc4d820905
Benjamin Otte <otte@gnome.org>
2007-02-16 09:46:19 +0100

    fix invalid code that caused crashes when parsing kerning tables
    Only Youtube seems to have those...
    Also updated Copyright headers

--- libswfdec/swfdec_font.c
+++ libswfdec/swfdec_font.c
@@ -1,7 +1,7 @@
 /* Swfdec
  * Copyright (C) 2003-2006 David Schleef <ds@schleef.org>
  *		 2005-2006 Eric Anholt <eric@anholt.net>
- *		      2006 Benjamin Otte <otte@gnome.org>
+ *		 2006-2007 Benjamin Otte <otte@gnome.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -237,7 +237,7 @@ tag_func_define_font (SwfdecSwfDecoder * s)
 static void
 swfdec_font_parse_kerning_table (SwfdecSwfDecoder *s, SwfdecFont *font, gboolean wide_codes)
 {
-  SwfdecBits *bits;
+  SwfdecBits *bits = &s->b;
   guint n_kernings, i;
 
   n_kernings = swfdec_bits_get_u16 (bits);

fa8acc601252c3ee1a7f74f06e4b113ecdcb57ce
Benjamin Otte <otte@gnome.org>
2007-06-17 21:56:58 +0200

    adapt license to LGPL
    If anyone wanna complain about it, see http://sswf.sourceforge.net/SWFalexref.html

--- libswfdec/swfdec_tag.h
+++ libswfdec/swfdec_tag.h
@@ -1,21 +1,26 @@
-/* tags.h */
-/* Take from: */
-/* rfxswf.h
-
-   Headers for rfxswf.c and modules
-
-   Part of the swftools package.
-
-   Copyright (c) 2000, 2001 Rainer Böhme <rfxswf@reflex-studio.de>
- 
-   This file is distributed under the GPL, see file COPYING for details 
-
-*/
+/* Swfdec
+ * Copyright (C) 2003-2006 David Schleef <ds@schleef.org>
+ *		 2005-2006 Eric Anholt <eric@anholt.net>
+ *		 2006-2007 Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
 
 #ifndef __LIBSWFDEC_TAGS_H__
 #define __LIBSWFDEC_TAGS_H__
-
-/* Tag IDs (adopted from J. C. Kessels' Form2Flash) */
 
 typedef enum {
   SWFDEC_TAG_END                  = 0,

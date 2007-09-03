62f2e4615342ee03bfd8d24e7ebc46752731c2d0
Benjamin Otte <otte@gnome.org>
2007-09-03 14:15:43 +0200

    __constructor__ is a Flash 6 invention

--- libswfdec/swfdec_as_interpret.c
+++ libswfdec/swfdec_as_interpret.c
@@ -1884,8 +1884,10 @@ swfdec_action_extends (SwfdecAsContext *cx, guint action, const guint8 *data, gu
     return;
   swfdec_as_object_get_variable (super, SWFDEC_AS_STR_prototype, &proto);
   swfdec_as_object_set_variable (prototype, SWFDEC_AS_STR___proto__, &proto);
-  swfdec_as_object_set_variable_and_flags (prototype, SWFDEC_AS_STR___constructor__,
-      superclass, SWFDEC_AS_VARIABLE_HIDDEN);
+  if (cx->version > 5) {
+    swfdec_as_object_set_variable_and_flags (prototype, SWFDEC_AS_STR___constructor__,
+	superclass, SWFDEC_AS_VARIABLE_HIDDEN);
+  }
   SWFDEC_AS_VALUE_SET_OBJECT (&proto, prototype);
   swfdec_as_object_set_variable (SWFDEC_AS_VALUE_GET_OBJECT (subclass),
       SWFDEC_AS_STR_prototype, &proto);

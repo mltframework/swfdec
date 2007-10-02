67eafbfa12f1e5a7b0429c5048173b188c21fa26
Benjamin Otte <otte@gnome.org>
2007-10-02 20:16:29 +0200

    also warn about missing CAIRO_EXTEND_PAD when parsing morph patterns

--- libswfdec/swfdec_pattern.c
+++ libswfdec/swfdec_pattern.c
@@ -521,7 +521,13 @@ swfdec_pattern_parse_morph (SwfdecBits *bits, SwfdecSwfDecoder *dec)
       if (paint_style_type == 0x40 || paint_style_type == 0x42) {
 	SWFDEC_IMAGE_PATTERN (pattern)->extend = CAIRO_EXTEND_REPEAT;
       } else {
+#if 0
+	/* not implemented yet in cairo */
+	SWFDEC_IMAGE_PATTERN (pattern)->extend = CAIRO_EXTEND_PAD;
+#else
+	SWFDEC_FIXME ("CAIRO_EXTEND_PAD is not yet implemented");
 	SWFDEC_IMAGE_PATTERN (pattern)->extend = CAIRO_EXTEND_NONE;
+#endif
       }
       if (paint_style_type == 0x40 || paint_style_type == 0x41) {
 	SWFDEC_IMAGE_PATTERN (pattern)->filter = CAIRO_FILTER_BILINEAR;

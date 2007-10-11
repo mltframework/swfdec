35f353b7f4ae6c7a69aa307832eb6ef164468207
Pekka Lampila <pekka.lampila@iki.fi>
2007-10-11 10:42:36 +0300

    Add copyright header to swfdec_html_parser.c and remove an extra include

--- libswfdec/swfdec_html_parser.c
+++ libswfdec/swfdec_html_parser.c
@@ -1,3 +1,22 @@
+/* Swfdec
+ * Copyright (C) 2007 Pekka Lampila <pekka.lampila@iki.fi>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -7,7 +26,6 @@
 
 #include "swfdec_edittext_movie.h"
 #include "swfdec_as_strings.h"
-#include "swfdec_font.h"
 #include "swfdec_debug.h"
 
 typedef struct {

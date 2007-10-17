dca4a26e7bbbb1d524ea1dd9e94628cbae4e2056
Benjamin Otte <otte@gnome.org>
2007-10-17 20:10:12 +0200

    add the first signs of the flash security model

---
+++ libswfdec/swfdec_flash_security.c
@@ -0,0 +1,105 @@
+/* Swfdec
+ * Copyright (C) 2007 Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <string.h>
+#include "swfdec_flash_security.h"
+#include "swfdec_debug.h"
+#include "swfdec_security_allow.h"
+
+
+G_DEFINE_TYPE (SwfdecFlashSecurity, swfdec_flash_security, SWFDEC_TYPE_SECURITY)
+
+static SwfdecSecurity *
+swfdec_flash_security_allow (SwfdecSecurity *guard, SwfdecSecurity *key)
+{
+  if (SWFDEC_IS_SECURITY_ALLOW (key)) {
+    return g_object_ref (guard);
+  } else if (SWFDEC_IS_FLASH_SECURITY (key)) {
+    SwfdecFlashSecurity *fguard, *fkey, *ret;
+    fguard = SWFDEC_FLASH_SECURITY (guard);
+    fkey = SWFDEC_FLASH_SECURITY (key);
+
+    ret = g_object_new (SWFDEC_TYPE_FLASH_SECURITY, NULL);
+    ret->allow_local = fguard->allow_local && fkey->allow_local;
+    ret->allow_remote = fguard->allow_remote && fkey->allow_remote;
+    return SWFDEC_SECURITY (ret);
+  } else {
+    SWFDEC_ERROR ("unknown security %s, denying access", G_OBJECT_TYPE_NAME (key));
+    return NULL;
+  }
+}
+
+static gboolean
+swfdec_flash_security_allow_url (SwfdecSecurity *guard, const SwfdecURL *url)
+{
+  SwfdecFlashSecurity *sec = SWFDEC_FLASH_SECURITY (guard);
+
+  if (swfdec_url_has_protocol (url, "http")) {
+    return sec->allow_remote;
+  } else if (swfdec_url_has_protocol (url, "file")) {
+    return sec->allow_local;
+  } else {
+    SWFDEC_ERROR ("unknown protocol %s, denying access", swfdec_url_get_protocol (url));
+    return FALSE;
+  }
+}
+
+static void
+swfdec_flash_security_class_init (SwfdecFlashSecurityClass *klass)
+{
+  SwfdecSecurityClass *security_class = SWFDEC_SECURITY_CLASS (klass);
+
+  security_class->allow = swfdec_flash_security_allow;
+  security_class->allow_url = swfdec_flash_security_allow_url;
+}
+
+static void
+swfdec_flash_security_init (SwfdecFlashSecurity *sec)
+{
+  sec->allow_local = TRUE;
+  sec->allow_remote = TRUE;
+}
+
+/**
+ * swfdec_flash_security_new:
+ *
+ * Creates a new Security object that allows everything. These objects are used 
+ * by default when no other security object is in use. This is particularly 
+ * useful for script engines that are not security sensitive or code injection
+ * via debugging.
+ *
+ * Returns: a new #SwfdecSecurity object
+ **/
+SwfdecSecurity *
+swfdec_flash_security_new (void)
+{
+  static SwfdecSecurity *singleton = NULL;
+  
+  /* FIXME: not threadsafe */
+  if (singleton)
+    return g_object_ref (singleton);
+
+  singleton = g_object_new (SWFDEC_TYPE_FLASH_SECURITY, NULL);
+  g_object_add_weak_pointer (G_OBJECT (singleton), (gpointer) &singleton);
+  return singleton;
+}

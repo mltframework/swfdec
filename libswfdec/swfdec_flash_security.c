19d4a71aaf9967a70648324b143be5a743bf5d20
Benjamin Otte <otte@gnome.org>
2007-10-18 13:14:57 +0200

    special case guard == key

--- libswfdec/swfdec_flash_security.c
+++ libswfdec/swfdec_flash_security.c
@@ -32,13 +32,16 @@ G_DEFINE_TYPE (SwfdecFlashSecurity, swfdec_flash_security, SWFDEC_TYPE_SECURITY)
 static SwfdecSecurity *
 swfdec_flash_security_allow (SwfdecSecurity *guard, SwfdecSecurity *key)
 {
-  if (SWFDEC_IS_SECURITY_ALLOW (key)) {
+  if (guard == key) {
+    return g_object_ref (guard);
+  } else if (SWFDEC_IS_SECURITY_ALLOW (key)) {
     return g_object_ref (guard);
   } else if (SWFDEC_IS_FLASH_SECURITY (key)) {
     SwfdecFlashSecurity *fguard, *fkey, *ret;
     fguard = SWFDEC_FLASH_SECURITY (guard);
     fkey = SWFDEC_FLASH_SECURITY (key);
 
+    SWFDEC_FIXME ("merging flash securities - this should create the right type");
     ret = g_object_new (SWFDEC_TYPE_FLASH_SECURITY, NULL);
     ret->allow_local = fguard->allow_local && fkey->allow_local;
     ret->allow_remote = fguard->allow_remote && fkey->allow_remote;

8dd254cbecda15f4f0a7d6ae0908bd08444aa5b4
Benjamin Otte <otte@gnome.org>
2007-11-06 11:57:44 +0100

    implement MovieClipLoader.getProgress()
    also add stubs for unloadMovie()

--- libswfdec/swfdec_movie_clip_loader.c
+++ libswfdec/swfdec_movie_clip_loader.c
@@ -23,7 +23,10 @@
 
 #include "swfdec_movie_clip_loader.h"
 #include "swfdec_as_internal.h"
+#include "swfdec_as_strings.h"
 #include "swfdec_debug.h"
+#include "swfdec_decoder.h"
+#include "swfdec_player_internal.h"
 #include "swfdec_resource.h"
 
 
@@ -57,5 +60,46 @@ swfdec_movie_clip_loader_loadClip (SwfdecAsContext *cx, SwfdecAsObject *object,
   SWFDEC_AS_CHECK (SWFDEC_TYPE_MOVIE_CLIP_LOADER, &loader, "ss", &url, &target);
 
   swfdec_resource_load (SWFDEC_PLAYER (cx), target, url, SWFDEC_LOADER_REQUEST_DEFAULT, NULL, loader);
+}
+
+SWFDEC_AS_NATIVE (112, 102, swfdec_movie_clip_loader_unloadClip)
+void 
+swfdec_movie_clip_loader_unloadClip (SwfdecAsContext *cx, SwfdecAsObject *object,
+    guint argc, SwfdecAsValue *argv, SwfdecAsValue *rval)
+{
+  SWFDEC_FIXME ("implement");
+}
+
+SWFDEC_AS_NATIVE (112, 101, swfdec_movie_clip_loader_getProgress)
+void 
+swfdec_movie_clip_loader_getProgress (SwfdecAsContext *cx, SwfdecAsObject *object,
+    guint argc, SwfdecAsValue *argv, SwfdecAsValue *rval)
+{
+  SwfdecMovieClipLoader *loader;
+  SwfdecMovie *movie;
+  SwfdecAsObject *ret;
+  const char *target;
+  SwfdecResource *resource;
+  SwfdecAsValue loaded, total;
+
+  SWFDEC_AS_CHECK (SWFDEC_TYPE_MOVIE_CLIP_LOADER, &loader, "s", &target);
+
+  movie = swfdec_player_get_movie_from_string (SWFDEC_PLAYER (cx), target);
+  if (movie == NULL)
+    return;
+  ret = swfdec_as_object_new_empty (cx);
+  if (ret == NULL)
+    return;
+  SWFDEC_AS_VALUE_SET_OBJECT (rval, ret);
+  resource = swfdec_movie_get_own_resource (movie);
+  if (resource == NULL || resource->decoder == NULL) {
+    SWFDEC_AS_VALUE_SET_INT (&loaded, 0);
+    SWFDEC_AS_VALUE_SET_INT (&total, 0);
+  } else {
+    SWFDEC_AS_VALUE_SET_INT (&loaded, resource->decoder->bytes_loaded);
+    SWFDEC_AS_VALUE_SET_INT (&total, resource->decoder->bytes_total);
+  }
+  swfdec_as_object_set_variable (ret, SWFDEC_AS_STR_bytesLoaded, &loaded);
+  swfdec_as_object_set_variable (ret, SWFDEC_AS_STR_bytesTotal, &total);
 }
 

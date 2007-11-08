ff66817638e142937eb49269c326d6347870a3b5
Benjamin Otte <otte@gnome.org>
2007-11-08 19:40:15 +0100

    make description not cause a gtk-doc warning.
    The detection code should be better...

--- libswfdec/swfdec_as_frame.c
+++ libswfdec/swfdec_as_frame.c
@@ -217,8 +217,8 @@ typedef struct {
  *
  * Registers a function that guards a block of memory. When the function 
  * exits this block of memory, it will call this function. This can happen
- * either when the program counter leaves the guarded region, when the function
- * returns or when the context aborted due to an unrecoverable error.
+ * either when the program counter leaves the guarded region, when the 
+ * function returns or when the context aborted due to an unrecoverable error.
  **/
 void
 swfdec_as_frame_push_block (SwfdecAsFrame *frame, const guint8 *start, 

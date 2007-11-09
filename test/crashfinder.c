099d54c6ed81f2a249e61d81406b6e7e96817feb
Pekka Lampila <pekka.lampila@iki.fi>
2007-11-09 18:55:51 +0200

    Fix crashfinder crashing on empty files

--- test/crashfinder.c
+++ test/crashfinder.c
@@ -124,6 +124,8 @@ main (int argc, char **argv)
 	  MIN (max_per_advance, max_per_file - elapsed));
 
       advance = swfdec_player_get_next_event (player);
+      if (advance == -1)
+	break;
       swfdec_player_advance (player, advance);
 
       swfdec_player_render (player, cr, 0, 0, 0, 0);

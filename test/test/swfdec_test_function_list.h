9185793d14379606672581a282839a56ca53c31e
Benjamin Otte <otte@gnome.org>
2008-01-07 17:43:25 +0100

    This file is autogenerated
    oops

--- test/test/swfdec_test_function_list.h
+++
@@ -1,4 +0,0 @@
-SWFDEC_TEST_FUNCTION ("print", swfdec_test_print, 0)
-SWFDEC_TEST_FUNCTION ("Test_advance", swfdec_test_test_advance, 0)
-SWFDEC_TEST_FUNCTION ("Test_reset", swfdec_test_test_reset, 0)
-SWFDEC_TEST_FUNCTION ("Test", swfdec_test_test_new, swfdec_test_test_get_type)

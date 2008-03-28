552f3d6d72f093c3df56aa619e0745472b6097a2
Pekka Lampila <pekka.lampila@iki.fi>
2008-03-28 11:13:24 +0200

    Make the compiler build and add a copyright header

--- vivified/code/compiler.c
+++ vivified/code/compiler.c
@@ -1,5 +1,28 @@
-#include <glib.h>
+/* Vivified
+ * Copyright (C) 2008 Pekka Lampila <pekka.lampila@iki.fi>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <string.h>
+#include <swfdec/swfdec.h>
 
 typedef enum {
   STATUS_FAIL = -1,
@@ -530,7 +553,7 @@ main (int argc, char *argv[])
     g_scanner_get_next_token (scanner);
     g_print (":: %i, %i :: ", g_scanner_cur_line (scanner),
 	g_scanner_cur_position (scanner));
-    switch (scanner->token) {
+    switch ((int)scanner->token) {
       case G_TOKEN_SYMBOL:
 	g_print ("SYMBOL\n");
 	break;

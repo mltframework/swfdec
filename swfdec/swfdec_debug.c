4d624cfdf6f5264c241edf5f11bacfa23680ea1c
Benjamin Otte <otte@gnome.org>
2008-04-02 12:13:29 +0200

    add documentation to the debug messages

--- swfdec/swfdec_debug.c
+++ swfdec/swfdec_debug.c
@@ -1,7 +1,7 @@
 /* Swfdec
  * Copyright (C) 2003-2006 David Schleef <ds@schleef.org>
  *		 2005-2006 Eric Anholt <eric@anholt.net>
- *		 2006-2007 Benjamin Otte <otte@gnome.org>
+ *		 2006-2008 Benjamin Otte <otte@gnome.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -25,6 +25,64 @@
 
 #include <glib.h>
 #include "swfdec_debug.h"
+
+/**
+ * SWFDEC_ERROR:
+ * @...: a printf-style message
+ *
+ * Emits an error debugging message. Error messages are used to indicate 
+ * mal-formed content. Examples are invalid data in the Flash file or results
+ * of broken code. Note that error messages do not indicate that Swfdec stopped
+ * interpretation of the Flash file.
+ */
+
+/**
+ * SWFDEC_FIXME:
+ * @...: a printf-style message
+ *
+ * Emits a fixme debugging message. FIXMEs are used when it is known that a
+ * feature is not yet correctly implemented and could cause wrong behavior while
+ * playing back this Flash file.
+ */
+
+/**
+ * SWFDEC_WARNING:
+ * @...: a printf-style message
+ *
+ * Emits a warning debugging message. Warnings are used to indicate unusual
+ * situations. An example would be a wrong argument type passed to a native 
+ * script function. Warning messages are output by default on development 
+ * builds. So make sure they are important enough to warrant being printed.
+ */
+
+/**
+ * SWFDEC_INFO:
+ * @...: a printf-style message
+ *
+ * Outputs an informational debugging message. This debugging category should 
+ * be used for rare messages that contain useful information. An example is
+ * printing the version and default size of a newly loaded Flash file.
+ */
+
+/**
+ * SWFDEC_DEBUG:
+ * @...: a printf-style message
+ *
+ * Outputs a debugging message. This debugging category should be used whenever
+ * something happens that is worth noting, but not the default behavior. For
+ * default messages, use SWFDEC_LOG(). An example would be if a special argument
+ * was passed by a script to a native function.
+ */
+
+/**
+ * SWFDEC_LOG:
+ * @...: a printf-style message
+ *
+ * Outputs a logging message. This debugging category should be used whenever
+ * there is something that might be interesting to a developer inspecting a run
+ * of Swfdec. It is used for example to dump every parsed token of the Flash 
+ * file.
+ */
 
 static const char *swfdec_debug_level_names[] = {
   "NONE ",

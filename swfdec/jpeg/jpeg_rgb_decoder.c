ec0d0a2b9626d83b395a11329084423c43ee821a
Riccardo Magliocchetti <riccardo@datahost.it>
2008-05-31 13:11:36 +0200

    fix leak in error path

--- swfdec/jpeg/jpeg_rgb_decoder.c
+++ swfdec/jpeg/jpeg_rgb_decoder.c
@@ -57,7 +57,10 @@ int jpeg_decode_argb (uint8_t *data, int length, uint32_t **image,
   jpeg_decoder_addbits (dec, data, length);
   ret = jpeg_decoder_decode(dec);
 
-  if (!ret) return FALSE;
+  if (!ret) {
+    jpeg_decoder_free (dec);
+    return FALSE;
+  }
 
   jpeg_decoder_get_image_size (dec, width, height);
   *image = (uint32_t *)jpeg_decoder_get_argb_image (dec);

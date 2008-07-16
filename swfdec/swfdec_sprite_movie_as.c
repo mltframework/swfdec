35f005a0cf68b458790f2da1f5d31c643ea99385
Benjamin Otte <otte@gnome.org>
2008-07-16 15:46:39 +0200

    unset the right sandbox

--- swfdec/swfdec_sprite_movie_as.c
+++ swfdec/swfdec_sprite_movie_as.c
@@ -658,7 +658,7 @@ swfdec_sprite_movie_createEmptyMovieClip (SwfdecAsContext *cx, SwfdecAsObject *o
   movie = swfdec_movie_new (SWFDEC_PLAYER (cx), depth, parent, parent->resource, NULL, name);
 
   if (SWFDEC_IS_SPRITE_MOVIE (movie)) {
-    SwfdecSandbox *sandbox = movie->resource->sandbox;
+    SwfdecSandbox *sandbox = SWFDEC_SANDBOX (cx->global);
     SwfdecActor *actor = SWFDEC_ACTOR (movie);
     swfdec_sandbox_unuse (sandbox);
     swfdec_movie_initialize (movie);

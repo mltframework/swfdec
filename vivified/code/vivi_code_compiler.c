47e4dcfb90816b43a2f0a7005497edfc705660ca
Benjamin Otte <otte@gnome.org>
2008-07-21 10:23:09 +0100

    make vivi-compile have a -o option so automatic pooling can be disabled

--- vivified/code/vivi_code_compiler.c
+++ vivified/code/vivi_code_compiler.c
@@ -85,9 +85,6 @@ vivi_code_compiler_compile_script (ViviCodeCompiler *compiler,
 
   vivi_code_compiler_compile_statement (compiler, statement);
   vivi_code_compiler_take_code (compiler, vivi_code_asm_end_new ());
-
-  vivi_code_assembler_pool (compiler->assembler);
-  vivi_code_assembler_merge_push (compiler->assembler, G_MAXUINT);
 }
 
 void

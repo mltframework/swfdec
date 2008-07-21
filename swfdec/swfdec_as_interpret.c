899da3596bcd6286d8d2f3b87649a41b75975182
Benjamin Otte <otte@gnome.org>
2008-07-21 10:22:21 +0100

    handle invalid strings and use the empty string instead

--- swfdec/swfdec_as_interpret.c
+++ swfdec/swfdec_as_interpret.c
@@ -318,10 +318,13 @@ swfdec_action_push (SwfdecAsContext *cx, guint action, const guint8 *data, guint
       case 0: /* string */
 	{
 	  char *s = swfdec_bits_get_string (&bits, cx->version);
-	  if (s == NULL)
-	    return;
-	  SWFDEC_AS_VALUE_SET_STRING (swfdec_as_stack_push (cx), 
-	      swfdec_as_context_give_string (cx, s));
+	  if (s == NULL) {
+	    SWFDEC_AS_VALUE_SET_STRING (swfdec_as_stack_push (cx), 
+		SWFDEC_AS_STR_EMPTY);
+	  } else {
+	    SWFDEC_AS_VALUE_SET_STRING (swfdec_as_stack_push (cx), 
+		swfdec_as_context_give_string (cx, s));
+	  }
 	  break;
 	}
       case 1: /* float */
